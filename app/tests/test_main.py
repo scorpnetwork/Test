baa70ca6714854b145c8752c6212acb9a229b74c
Ala S Bakhamis <32382009+ScorpSupport@users.noreply.github.com>
2025-01-27 03:40:55 +0300

    Create test_main.py

---
+++ app/tests/test_main.py
@@ -0,0 +1,18 @@
+# tests/test_example.py
+
+import unittest
+from ..main import add, subtract
+
+class TestExample(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(-1, 1), -2)
+        self.assertEqual(subtract(-1, -1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
