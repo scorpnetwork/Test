d75576f13936a30474d25f366dd2396c47cd810c
cool-guy <aam-10>
2025-01-29 11:28:04 +0200

    Created login funtionality, added documentation for the /login and /register endpoints. Still need to wirte JWT functions

--- app/main.py
+++ app/main.py
@@ -56,10 +56,6 @@ class UserUpdate():
 class UserLogin(UserBase):
     plain_password: str
 
-    def authenticate():
-        pass
-
-
 
 # Pydantic models will need their own file in the future ->
 class LoginResponse(BaseModel):
@@ -83,13 +79,40 @@ def on_startup():
 async def home():
     return {"Hello": "World"}
 
-@app.post("/login", response_model=UserPublic)
-async def login(): # async def with non-blocking IO operations
-    jwt_token = jwt
-    return jwt_token
+@app.post("/login", response_model=UserPublic, responses={409: {"description": "Wrong email or password"}})
+async def login(user: UserLogin, session: SessionDep): # async def with non-blocking IO operations
+    """
+    Logs a user in, upon success, the user is authenticated and a JWT is returned.
 
-@app.post("/register", response_model=UserPublic)
+    - **email**: The email of the user from the HTML form.
+    - **plain_password**: The plain password from the HTML form.
+    """
+    
+    validated_user = UserLogin.model_validate(user)
+
+    stmt = select(User).where(User.email == validated_user.email)
+    result = session.exec(stmt).first()
+
+    print(result)
+    
+    if not result:
+        raise HTTPException(status_code=409, detail="Wrong email or password")
+    
+    # Verify if the provided plain password matches the hashed password in the database
+    if not pwd_context.verify(validated_user.plain_password, result.hashed_password):
+        raise HTTPException(status_code=409, detail="Wrong email or password")
+
+    return UserPublic(email=validated_user.email)
+
+@app.post("/register", response_model=UserPublic, responses={409: {"description": "Email already registered"}})
 def register(user: UserCreate, session: SessionDep):
+    """
+    Creates a new account, upon success, a user is register in the database, authenticated and a JWT is returned.
+
+    - **email**: The email of the user from the HTML form.
+    - **plain_password**: The plain password from the HTML form.
+    """
+
     validated_user = UserCreate.model_validate(user)
     
     db_user = User(email=validated_user.email, hashed_password=hash_password(validated_user.plain_password))
@@ -107,4 +130,4 @@ def register(user: UserCreate, session: SessionDep):
         ) from e
 
     session.refresh(db_user)
-    return db_use
+    return db_user
